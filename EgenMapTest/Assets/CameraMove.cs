f20bbf264178437ae32cdbc6d7ea85df447da1ca
Alex Skov Klitgaard <alex.klitgaard@gmail.com>
2016-12-20 11:03:31 +0100

    New images + UI correction + Not done android fix code

--- EgenMapTest/Assets/CameraMove.cs
+++ EgenMapTest/Assets/CameraMove.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class CameraMove : MonoBehaviour {
-	
+    
     /// <summary>
     /// Unity method
     /// Called when attached game object gets enabled
@@ -15,16 +15,23 @@ public class CameraMove : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-	    //if (Input.touchCount == 1 && Application.platform == RuntimePlatform.Android)
-     //   {
-     //       Touch touch1 = Input.GetTouch(0);
+        //if (Input.touchCount == 1 && Application.platform == RuntimePlatform.Android)
+        //   {
+        //       Touch touch1 = Input.GetTouch(0);
 
-     //       if (touch1.phase == TouchPhase.Moved)
-     //       {
-     //           Vector3 newPos = new Vector3(-touch1.deltaPosition.x, 0, -touch1.deltaPosition.y) * Time.deltaTime * 150;
+        //       if (touch1.phase == TouchPhase.Moved)
+        //       {
+        //           Vector3 newPos = new Vector3(-touch1.deltaPosition.x, 0, -touch1.deltaPosition.y) * Time.deltaTime * 150;
 
-     //           transform.position += newPos;
-     //       }
-     //   }
+        //           transform.position += newPos;
+        //       }
+        //   }
+
+        //transform.position = new Vector3(User.Instance.gameObject.transform.position.x, 47.5f, User.Instance.gameObject.transform.position.z);
 	}
+
+    private void LateUpdate()
+    {
+        gameObject.transform.rotation = Quaternion.Euler(90, 0, 0);
+    }
 }
